c967bb3839f2a9c7f119d5d7e16c734b547d8551
andrscc <andrescc143@gmail.com>
2023-04-29 22:03:51 -0500

    The components 'movies', 'movies-slider' and 'movie-details' were created and are ready to use

--- playvie-frontend/src/app/app.module.ts
+++ playvie-frontend/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { MoviesHighlightComponent } from './components/movies-highlight/movies-highlight.component';
+import { MoviesComponent } from './components/movies/movies.component';
+import { MoviesSliderComponent } from './components/movies-slider/movies-slider.component';
+import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    MoviesHighlightComponent
+    MoviesHighlightComponent,
+    MoviesComponent,
+    MoviesSliderComponent,
+    MovieDetailsComponent
   ],
   imports: [
     BrowserModule,
