10f13aeea9b035f8e1a8ed2dbf68cf7cfd920293
andrscc <andrescc143@gmail.com>
2023-04-29 21:56:43 -0500

    The movies-highlight component was created

--- playvie-frontend/src/app/app.module.ts
+++ playvie-frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { MoviesHighlightComponent } from './components/movies-highlight/movies-highlight.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    MoviesHighlightComponent
   ],
   imports: [
     BrowserModule,
---
+++ playvie-frontend/src/app/components/movies-highlight/movies-highlight.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movies-highlight',
+  templateUrl: './movies-highlight.component.html',
+  styleUrls: ['./movies-highlight.component.css']
+})
+export class MoviesHighlightComponent {
+
+}
