20e8c9967594498d3c9c74620e9eb5f5e0857ed3
andrscc <andrescc143@gmail.com>
2023-07-01 18:03:01 -0500

    Feature: The Header component was deleted; The Movies component was deleted; The placeholder for the search input component was changed;

--- playvie-frontend/src/app/app.module.ts
+++ playvie-frontend/src/app/app.module.ts
@@ -5,10 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { MoviesHighlightComponent } from './components/movies-highlight/movies-highlight.component';
-import { MoviesComponent } from './components/movies/movies.component';
 import { MoviesSliderComponent } from './components/movies-slider/movies-slider.component';
 import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
-import { HeaderComponent } from './components/header/header.component';
 import { SearchInput } from './components/search-input/search-input.component';
 import { MainButton } from './components/main-button/main-button.component';
 import { CardMovie } from './components/card-movie/card-movie.component';
@@ -17,9 +15,7 @@ import { CardMovie } from './components/card-movie/card-movie.component';
   declarations: [
     AppComponent,
     NavBarComponent,
-    HeaderComponent,
     MoviesHighlightComponent,
-    MoviesComponent,
     MoviesSliderComponent,
     MovieDetailsComponent,
     SearchInput,
